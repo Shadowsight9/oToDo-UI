66b2226efe579e51578da2c5e279db8674a68f32
Shadowsight9 <huangzheng@shu.edu.cn>
2022-02-03 22:16:17 +0800

    feat: add reactive data for login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,51 +1,71 @@
 <script setup lang="ts">
 import oToDoLogo from '@/assets/images/oToDo-logo.jpg'
-import { ref } from 'vue'
-const isLogin = ref(true)
+import LoggingIn from '@/assets/images/logging-in.gif'
+import { ref, reactive } from 'vue'
 
-function changeMode() {
+const isLogin = ref(true)
+const isLoading = ref(false)
+
+const loginForm = reactive({
+  username: '',
+  password: '',
+})
+
+const changeMode = () => {
   isLogin.value = !isLogin.value
+}
+const handleLogin = async () => {
+  isLoading.value = true
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1500)
 }
 </script>
 <template>
   <div class="wrapper">
     <form v-if="isLogin" class="login-box">
-      <img :src="oToDoLogo" alt="oToDo-logo" />
+      <img
+        class="img-loading"
+        :class="{ hidden: isLoading }"
+        :src="LoggingIn"
+        alt="logging-in"
+      />
+      <img class="img-logo" :src="oToDoLogo" alt="oToDo-logo" />
       <h1>oToDo Login</h1>
-      <div class="input-wrapper">
+      <div class="input-group">
+        <div class="group-row">
+          <p>请输入有效的电子邮件地址、电话号码或 Skype 用户名</p>
+          <input
+            id="username"
+            v-model="loginForm.username"
+            type="text"
+            name="username"
+            :disabled="isLoading"
+            placeholder="用户名、电子邮件、电话或 Skype"
+          />
+        </div>
+        <div class="group-row group-wearning">
+          <p>请输入有效的密码</p>
+          <input
+            id="password"
+            v-model="loginForm.password"
+            type="password"
+            name="password"
+            :disabled="isLoading"
+            placeholder="密码"
+          />
+        </div>
+        <p class="change-mode">没有账户？<a @click="changeMode">创建一个</a></p>
+      </div>
+
+      <div class="button-group">
         <input
-          id="usrname"
-          type="text"
-          name="usrname"
-          placeholder="电子邮件、电话或 Skype"
-        />
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="密码"
+          type="submit"
+          value="登录"
+          :disabled="isLoading"
+          @click.prevent="handleLogin"
         />
       </div>
-      <p>没有账户？<a @click="changeMode()">创建一个</a></p>
-      <div class="buttons"><input type="submit" value="登录" /></div>
-    </form>
-    <form v-else class="register-box">
-      <img :src="oToDoLogo" alt="oToDo-logo" />
-      <h1>oToDo Register</h1>
-
-      <input
-        id="usrname"
-        type="text"
-        name="usrname"
-        placeholder="电子邮件、电话或skype"
-      />
-      <input id="password" type="password" name="password" placeholder="密码" />
-      <input type="password" name="password-confirm" placeholder="确认密码" />
-
-      <p>
-        已经有账户了？<a class="change-mode" @click="changeMode()">转去登录</a>
-      </p>
-      <div class="buttons"><input type="submit" value="注册" /></div>
     </form>
   </div>
 </template>
@@ -57,61 +77,110 @@ function changeMode() {
   align-items: center;
   justify-content: center;
   background: linear-gradient(to right, #a8cecf, #e6ae8c);
-  .login-box,
-  .register-box {
-    min-height: 330px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff;
     max-width: 380px;
     width: calc(100% - 40px);
-    padding: 44px;
-    background-color: #fff;
+    min-height: 330px;
     box-shadow: 0 2px 6px rgb(0 0 0 / 20%);
-    img {
+    padding: 0 40px;
+
+    .img-loading {
+      width: calc(100% + 80px);
+      visibility: hidden;
+    }
+    .hidden {
+      visibility: unset;
+    }
+
+    .img-logo {
       height: 80px;
+      width: min-content;
       object-fit: contain;
+      margin-top: 40px;
     }
     h1 {
-      margin: 10px 0;
-      font-size: 20px;
+      margin: 10px 0 0 0;
+      font-size: 24px;
     }
-    input {
-      display: block;
-      width: 100%;
-      height: 36px;
-      outline: none;
-      border: 0;
-      border-bottom: 1px solid rgba(0, 0, 0, 0.6);
-      margin: 10px 0;
-      padding: 6px 0;
-      font-size: 16px;
-      &:hover {
-        border-bottom: 1px solid rgba(0, 0, 0, 0.8);
+    .img-logo,
+    h1 {
+      align-self: flex-start;
+    }
+
+    .input-group {
+      width: 98%;
+      .group-row {
+        margin: 20px 0;
+        p {
+          font-size: 14px;
+          color: #e81123;
+          display: none;
+        }
+        input {
+          display: block;
+          width: 100%;
+          height: 25px;
+          padding: 6px;
+          border: 0;
+          border-bottom: 1px solid rgba(0, 0, 0, 0.6);
+          outline: none;
+          font-size: 16px;
+          &:hover {
+            border-bottom: 1px solid rgba(0, 0, 0, 0.9);
+          }
+        }
+      }
+
+      .group-wearning {
+        p {
+          display: unset;
+        }
+        input {
+          border-bottom: 1.5px solid #e81123;
+          &:hover {
+            border-bottom: 1.5px solid #e81123;
+          }
+        }
+      }
+
+      .change-mode {
+        font-size: 14px;
+        margin: 0;
+        a {
+          color: #0067b8;
+          cursor: pointer;
+          &:hover {
+            text-decoration: underline;
+            color: #666;
+          }
+        }
       }
     }
-    p {
-      font-size: 14px;
-      a {
-        color: #0067b8;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
-          color: #666;
-        }
-        &:active {
-          transform: scale(0.7);
-        }
-      }
-    }
-    .buttons {
+
+    .button-group {
+      margin-top: 10px;
+      margin-bottom: 40px;
+      align-self: flex-end;
       input {
         background-color: #0067b8;
         color: #fff;
         height: 36px;
         width: 100px;
-        position: relative;
+        border: 0;
         float: right;
         &:hover {
           background-color: #005da6;
           cursor: pointer;
+        }
+        &:disabled {
+          background-color: #d1d1d1;
+          color: #b3b3b3;
+          cursor: wait;
         }
       }
     }
