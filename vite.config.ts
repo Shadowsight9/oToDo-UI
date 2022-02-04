58ddda4db5f9e97245e0d6ffef2dafccb8c68b13
Shadowsight9 <huangzheng@shu.edu.cn>
2022-02-04 12:51:29 +0800

    refactor: add .env file and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,42 @@
-import { defineConfig } from 'vite'
+import type { UserConfig, ConfigEnv } from 'vite'
+import { loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // 指定要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // 执行icon name的格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = env
+
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'src'),
+        },
+      ],
+    },
+    server: {
+      https: false,
+      // Listening on all local IPs
+      host: true,
+      port: Number(VITE_PORT),
+      // Load proxy configuration from .env
+      // proxy: createProxy(VITE_PROXY),
+    },
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // 指定要缓存的图标文件夹
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // 执行icon name的格式
+        symbolId: 'icon-[dir]-[name]',
+      }),
     ],
-  },
-})
+  }
+}
