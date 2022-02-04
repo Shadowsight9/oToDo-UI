f513732baa32b0e93b7050ddede170382d1925fc
Shadowsight9 <huangzheng@shu.edu.cn>
2022-02-04 12:22:52 +0800

    refactor: update svg icon path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     vue(),
     createSvgIconsPlugin({
       // 指定要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       // 执行icon name的格式
       symbolId: 'icon-[dir]-[name]',
     }),
